74813353c1cf06413329ad0362546e092ad3c8d5
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-17 12:25:03 +0900

    edit_message.js add_formData

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,15 @@
 $(function(){
   $('.message-form').on('submit', function(e) {
     e.preventDefault();
-    message = $('#upload-text').val();
-    console.log(message);
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   });
 });
