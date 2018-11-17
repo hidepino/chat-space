46f5acefe4ba71550a6ec6948d8024080d64b858
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-17 18:57:34 +0900

    add_done_method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,33 @@
 $(function(){
+  function buildhtml(message){
+    var html = `<div class="message">
+                  <div class="message__info">
+                    <div class="message__info__username">
+                      ${ message.user_name }
+                    </div>
+                    <div class="message__info__posttime">
+                      ${ message.posttime }
+                    </div>
+                  </div>
+                  <div class="message__detail">
+                    ${message.body}
+                  </div>
+                </div>`
+    return html;
+  }
+
+  function buildimage(message){
+    var image = `<div class="message__image">
+                  <img src=${message.image} width="200" height="100">
+                </div>`
+    return image;
+  }
+
+
   $('.message-form').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +35,21 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      console.log(data.image)
+      var html = buildhtml(data);
+      $('.messages').append(html);
+      if (data.image != null ){
+        var image = buildimage(data);
+        $('.message:last').append(image);
+      };
+      $('#upload-text').val('');
+      // $(function(){
+      //   $(".messages").animate({
+      //     scrollTop: $('.messages')[bottom-message].scrollHeight
+      //   })
+      // });
     })
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
 json.body       @message.body
 json.user_name  @message.user.name
 json.posttime   @message.created_at
-json.image      @message.image
+json.image      @message.image.url
