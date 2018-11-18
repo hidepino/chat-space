1a9b101cf2bd2b1efaa4da3088e759613da12db8
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-18 17:28:41 +0900

    add_html

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,6 +9,13 @@ $(function(){
     search_list.append(list);
   }
 
+  function appendNoUser(word){
+    var list =`<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${word}</p>
+              </div>`;
+    search_list.append(list);
+  }
+
   var input = $('#user-search-field');
   input.on('keyup', function(){
     var name = input.val();
@@ -18,11 +25,10 @@ $(function(){
       url: "/users",
       data: { keyword: name },
       dataType: 'json',
-      processData: false,
+      // processData: false,
       contentType: false
     })
     .done(function(users){
-      // console.log(data)
       $(".chat-group-user").remove();
       if (users.length !== 0){
         users.forEach(function(user){
@@ -31,6 +37,9 @@ $(function(){
       } else {
         appendNoUser("一致するユーザーはいませんでした。")
       }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
     })
   });
 });
