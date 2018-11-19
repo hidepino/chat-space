8bc7ad45e535820d48d8c5400e44b0ab5f834c93
高橋英明 <kaenriter.manzai@gamil.com>
2018-11-19 18:00:36 +0900

    add_delete

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -16,9 +16,21 @@ $(function(){
     search_list.append(list);
   }
 
+  var chatmember = $("#chat-group-users");
+
+  function appendChatmenber(username, user_id){
+    var list = `<div class='chat-group-user clearfix js-chat-member'>
+                <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                <p class='chat-group-user__name'>${username}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    chatmember.append(list);
+  }
+
   var input = $('#user-search-field');
   input.on('keyup', function(){
     var name = input.val();
+    $('#user-search-result').empty();
 
     $.ajax({
       type: 'GET',
@@ -29,7 +41,6 @@ $(function(){
       contentType: false
     })
     .done(function(users){
-      $(".chat-group-user").remove();
       if (users.length !== 0){
         users.forEach(function(user){
           appendUser(user);
@@ -42,4 +53,17 @@ $(function(){
       alert('ユーザー検索に失敗しました');
     })
   });
+    search_list.on("click", ".chat-group-user__btn--add", function(){
+      // alert("");
+      var index = $(".chat-group-user__btn--add").index(this);
+      var username = $(".chat-group-user__btn--add").eq(index).attr("data-user-name");
+      var user_id = $(".chat-group-user__btn--add").eq(index).attr("data-user-id");
+      appendChatmenber(username, user_id);
+      $(this).parent().remove();
+      $("#user-search-field").val('');
+    })
+    $("#chat-group-users").on("click", ".js-remove-btn", function(){
+      var index = $(".js-remove-btn").index(this) + 1;
+      $(".chat-group-user.clearfix.js-chat-member").eq(index).remove();
+    })
 });
